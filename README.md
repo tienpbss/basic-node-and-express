f1077727a1c9e861cb575a08b909a8f69a9bd92d
tienpbss <tienphung.cn07@gmail.com>
2023-02-25 11:25:46 +0700

    pratice 1 2 ffc

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 # Basic Node and Express
 
 This is the boilerplate code for the Basic Node and Express Challenges. Instructions for working on these challenges start at https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/
-=======
-# basic-node-and-express
-Study node and express in freecodecamp
->>>>>>> 7ff9142d5ba9aae60f1a0ba8eb22bab3d0f534fc
--- myApp.js
+++ myApp.js
@@ -1,17 +1,25 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+// 1. Meet the Node console
+// app.get('/', (req, res) => {
+//     res.send('Hello Express');
+// })
 
+// 3. Serve Static Assets
 
+let publicPath = __dirname + '/public';
+app.use('/', express.static(publicPath));
 
+// 2. Serve an HTML File
+console.log(__dirname)
+let htmlPath = __dirname + '/views/index.html';
 
-
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.sendFile(htmlPath);
+})
 
 
 
